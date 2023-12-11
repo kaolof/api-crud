c5c3cb4fa54100460f269589275355da0bf1a991
Kao Lo <kaochilanlo@gmail.com>
2023-12-11 13:16:11 -0400

    creando primeras rutas

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log("hello")
+const express= require('express')
+const morgan= require('morgan')
+const app=express()
+
+app.use(morgan('dev'))
+
+app.get('/clients', (req,res)=>{
+    res.send('obteniendo clients')
+})
+app.post('/clients', (req,res)=>{
+    res.send('creando clients')
+})
+app.put('/clients', (req,res)=>{
+    res.send('actualizando clients')
+})
+app.delete('/clients', (req,res)=>{
+    res.send('eliminando clients')
+})
+
+app.get('/clients/:id', (req,res)=>{
+    res.send('obtenido client')
+})
+
+app.listen(3000)
+console.log(`server on port ${3000}`)
